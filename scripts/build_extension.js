5863d270a64085504d50d2e4ddc09d157a41fb53
adam <athornton@gmail.com>
2017-05-16 13:39:21 -0700

    Remove all the Javascript (JL 0.21 changes)

--- scripts/build_extension.js
+++
@@ -1,7 +0,0 @@
-var buildExtension = require('@jupyterlab/extension-builder').buildExtension;
-
-buildExtension({
-        name: 'sqre_labkubespawner',
-        entry: './lib/plugin.js',
-        outputDir: './sqre_labkubespawner/static'
-});
